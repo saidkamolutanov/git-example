c82b09137fed007f8994f4fd7e2148c68c3f092d
saidkamol <saidkamolutanov@gmail.com>
2026-03-04 20:36:58 +0500

    Created two new class

---
+++ src/OOP/Employee.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class Employee {
+
+    String firstname;
+    String lastsurname;
+
+//    public Employee(){
+//        System.out.println("I am learning constructor.");
+//    }
+
+//    public Employee(String txt){
+//        System.out.println(txt);
+//    }
+
+    public Employee(String firstname, String lastsurname){
+        System.out.println(firstname + " " + lastsurname);
+    }
+}
---
+++ src/OOP/Main.java
@@ -0,0 +1,17 @@
+package OOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //constructor
+
+        Employee ishchi_1 = new Employee("Name", "Surname");
+
+//        ishchi_1.firstname = "ishchi 1 name";
+//        ishchi_1.lastsurname = "ishchi 1 surname";
+
+//        System.out.println(ishchi_1.firstname);
+//        System.out.println(ishchi_1.lastsurname);
+    }
+}
